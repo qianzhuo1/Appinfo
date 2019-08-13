334dfade495b484842c784f205fd64378eda7452
qianzhuo1 <1719860490@qq.com>
2019-08-13 15:48:54 +0800

    2019-8-13

---
+++ src/cn/appsys/pojo/User.java
@@ -0,0 +1,16 @@
+package cn.appsys.pojo;
+
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String devCode;
+	private String devName;
+	private String devPassword;
+	private String devEmail;
+	private String devInfo;
+	private int createdBy;
+	private Date creationDate;
+	private int modifyBy;
+	private Date modifyDate;
+}
